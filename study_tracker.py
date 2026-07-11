cef98f33ce4777b3854b08e25e84cbd4072ab9ef
Ali <h84kht4m96@privaterelay.appleid.com>
2026-07-11 13:07:03 +0100

    Delete study_tracker.py

--- study_tracker.py
+++
@@ -1,9 +0,0 @@
-topic = input("enter topic naame: ")
-conf = null
-while conf not in range(0,6) 
-  conf = int(input("rate your confidence from 1-5: ")
-
-nextdate = input("When would you like to revise next?: ")
-print(f'Your confidence level in {topic} is {conf} and your next study session is on {nextdate}.)
-if conf <=2:
-  print(f'I suggest you revise {topic} soon.')
